3415f0c9deab23a398cc37755c38cb87f18886c0
Cornell <1878996697@qq.com>
2026-05-25 16:17:21 +0800

    campus: add Spring Boot campus sample

---
+++ springboot-campus/src/main/java/top/kenan/campus/SpringbootCampusApplication.java
@@ -0,0 +1,13 @@
+package top.kenan.campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCampusApplication.class, args);
+    }
+
+}
---
+++ springboot-campus/src/test/java/top/kenan/campus/SpringbootCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kenan.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
